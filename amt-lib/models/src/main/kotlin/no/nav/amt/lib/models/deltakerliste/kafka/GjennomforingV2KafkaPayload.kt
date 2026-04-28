6f28529a1dae799fe5ca3c8ef24bec0c4807e920
Izis Frøyhov <izis.froyhov@nav.no>
2026-04-28 12:09:30 +0200

    Hopper over gjennomføring som er korrupt

--- amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
+++ amt-lib/models/src/main/kotlin/no/nav/amt/lib/models/deltakerliste/kafka/GjennomforingV2KafkaPayload.kt
@@ -133,7 +133,7 @@ sealed interface GjennomforingV2KafkaPayload {
         // I tilfellet vi har noen gjennomføringer som feiler
         val gjennomforingBlacklist = setOf<UUID>(
             UUID.fromString("e1be2f30-bb71-4938-86fb-582f4bc8a8c7"),
-            UUID.fromString("f88e1ce6242d12f95945fbe1cecaac69"),
+            UUID.fromString("a7fa2b1d88b067855d4c5f67b88099b7"),
         )
 
         val direktetiltak =
